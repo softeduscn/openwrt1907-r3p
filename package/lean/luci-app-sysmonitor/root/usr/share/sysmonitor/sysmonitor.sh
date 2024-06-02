f91bfda7d6b8bc58f88a9ae6a53f7e4fca0b22b3
softeduscn <softeduscn@gmail.com>
2024-06-02 13:00:09 +0800

    Update

--- package/lean/luci-app-sysmonitor/root/usr/share/sysmonitor/sysmonitor.sh
+++ package/lean/luci-app-sysmonitor/root/usr/share/sysmonitor/sysmonitor.sh
@@ -303,13 +303,21 @@ while [ "1" == "1" ]; do
 		for i in $prog
 		do
 			progsh=$i'.sh'
-			if [ "$(pgrep -f $progsh|wc -l)" == 0 ]; then
-				progrun='/tmp/'$i'.run'
-				progpid='/tmp/'$i'.pid'
-				[ -f $progrun ] && rm $progrun
-				[ -f $progpid ] && rm $progpid
-				$APP_PATH/$progsh &
-			fi
+			progpid='/tmp/'$i'.pid'
+			arg=$(cat $progpid)
+			case $arg in
+				0)
+					progrun='/tmp/'$i'.run'
+					[ -f $progrun ] && rm $progrun
+					[ -f $progpid ] && rm $progpid
+					$APP_PATH/$progsh &
+					;;
+				1)
+					;;
+				*)
+					killall $progsh
+					;;
+			esac	
 		done
 		if [ -f /tmp/wan6.sign ]; then
 			rm /tmp/wan6.sign
