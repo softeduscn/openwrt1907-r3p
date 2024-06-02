e23dc6a9024638b96a935a5bfc85abe858004ab1
softeduscn <softeduscn@gmail.com>
2024-06-02 13:28:56 +0800

    Update

--- package/lean/luci-app-sysmonitor/root/usr/share/sysmonitor/sysmonitor.sh
+++ package/lean/luci-app-sysmonitor/root/usr/share/sysmonitor/sysmonitor.sh
@@ -304,9 +304,11 @@ while [ "1" == "1" ]; do
 		do
 			progsh=$i'.sh'
 			progpid='/tmp/'$i'.pid'
+			[ ! -f $progpid ] && echo 0 > $progpid
 			arg=$(cat $progpid)
 			case $arg in
 				0)
+					[ "$(pgrep -f $progsh|wc -l)" != 0 ] && killall $progsh
 					progrun='/tmp/'$i'.run'
 					[ -f $progrun ] && rm $progrun
 					[ -f $progpid ] && rm $progpid
@@ -316,6 +318,7 @@ while [ "1" == "1" ]; do
 					;;
 				*)
 					killall $progsh
+					echo 0 > $progpid
 					;;
 			esac	
 		done
